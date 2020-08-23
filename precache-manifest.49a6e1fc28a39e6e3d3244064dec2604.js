899bafca996dcc97d735e8adc553bd53dd2bc22d
reah350 <h4riis31@gmail.com>
2020-08-23 13:10:12 +0200

    Updates

---
+++ precache-manifest.49a6e1fc28a39e6e3d3244064dec2604.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c47050bc60e8b2db07afabf58c856f",
+    "url": "/Bosancica/index.html"
+  },
+  {
+    "revision": "5fd7b4fe82af91ea5a0e",
+    "url": "/Bosancica/static/css/2.3699ddb1.chunk.css"
+  },
+  {
+    "revision": "95102413e718bf9f3443",
+    "url": "/Bosancica/static/css/main.8caf4e27.chunk.css"
+  },
+  {
+    "revision": "5fd7b4fe82af91ea5a0e",
+    "url": "/Bosancica/static/js/2.e22680f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Bosancica/static/js/2.e22680f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95102413e718bf9f3443",
+    "url": "/Bosancica/static/js/main.b03d3faf.chunk.js"
+  },
+  {
+    "revision": "69f248668259e451c683",
+    "url": "/Bosancica/static/js/runtime-main.86a46033.js"
+  },
+  {
+    "revision": "99f9a0f0e337300bdd5849002fb8197d",
+    "url": "/Bosancica/static/media/1.99f9a0f0.JPG"
+  },
+  {
+    "revision": "edd2e67567fc245a83ad2931cd351da9",
+    "url": "/Bosancica/static/media/1.edd2e675.JPG"
+  },
+  {
+    "revision": "24ba4316fb8327949d55a636b0773618",
+    "url": "/Bosancica/static/media/10.24ba4316.JPG"
+  },
+  {
+    "revision": "ff8ce1de6be39c0340967473a810643f",
+    "url": "/Bosancica/static/media/11.ff8ce1de.JPG"
+  },
+  {
+    "revision": "5b5ba43e6723d6553f40b0f0b434e423",
+    "url": "/Bosancica/static/media/12.5b5ba43e.JPG"
+  },
+  {
+    "revision": "1ed7cd827421ffb22557df7ac32d6b2f",
+    "url": "/Bosancica/static/media/13.1ed7cd82.JPG"
+  },
+  {
+    "revision": "f3f1987f15d38c683edd09875a5e5c48",
+    "url": "/Bosancica/static/media/14.f3f1987f.JPG"
+  },
+  {
+    "revision": "5742b8b44a982d7aace5acc3a147a671",
+    "url": "/Bosancica/static/media/15.5742b8b4.JPG"
+  },
+  {
+    "revision": "dad9c6b97b38d68f51b6147e4d25f95a",
+    "url": "/Bosancica/static/media/16.dad9c6b9.JPG"
+  },
+  {
+    "revision": "bcb2bc3900fffd2775468bfe3d9b7a5a",
+    "url": "/Bosancica/static/media/17.bcb2bc39.JPG"
+  },
+  {
+    "revision": "11e7e35077c5b55beab0422585493d27",
+    "url": "/Bosancica/static/media/18.11e7e350.JPG"
+  },
+  {
+    "revision": "dd8040a4da23863e2c40c6781f2bb739",
+    "url": "/Bosancica/static/media/19.dd8040a4.JPG"
+  },
+  {
+    "revision": "032017c75b8b9a4b5b2dd5fcb0c3ec44",
+    "url": "/Bosancica/static/media/2.032017c7.JPG"
+  },
+  {
+    "revision": "b0d21caf87e196d8b6813bf63e5edbea",
+    "url": "/Bosancica/static/media/2.b0d21caf.JPG"
+  },
+  {
+    "revision": "b54b56a555e107e9c343a735df74835d",
+    "url": "/Bosancica/static/media/2.b54b56a5.JPG"
+  },
+  {
+    "revision": "cfd5172c2abfd8957f9e91a12ba463fc",
+    "url": "/Bosancica/static/media/20.cfd5172c.JPG"
+  },
+  {
+    "revision": "ec5fd7fc6e11712bfc3cca6e3f684311",
+    "url": "/Bosancica/static/media/21.ec5fd7fc.JPG"
+  },
+  {
+    "revision": "b36e15e7adc4d9acba99dfb8e861b2fd",
+    "url": "/Bosancica/static/media/22.b36e15e7.JPG"
+  },
+  {
+    "revision": "662ab74102292c1f33a9390e23026862",
+    "url": "/Bosancica/static/media/23.662ab741.JPG"
+  },
+  {
+    "revision": "5be812ce99db9a70876fa8d48d0ad79c",
+    "url": "/Bosancica/static/media/24.5be812ce.JPG"
+  },
+  {
+    "revision": "b5a9ca239cd3b7d2121133bfa5dddbc1",
+    "url": "/Bosancica/static/media/25.b5a9ca23.JPG"
+  },
+  {
+    "revision": "fc36eeb884937e62691d0de2cd6179f8",
+    "url": "/Bosancica/static/media/26.fc36eeb8.JPG"
+  },
+  {
+    "revision": "f44c84e45afec6a966b46ac656859530",
+    "url": "/Bosancica/static/media/27.f44c84e4.JPG"
+  },
+  {
+    "revision": "e4f250b1691f5df10fca5eac4cc55e64",
+    "url": "/Bosancica/static/media/28.e4f250b1.JPG"
+  },
+  {
+    "revision": "062950d9ce462a942e658443c0986c55",
+    "url": "/Bosancica/static/media/29.062950d9.JPG"
+  },
+  {
+    "revision": "29574bb4d522506134afe69f07ba3412",
+    "url": "/Bosancica/static/media/3.29574bb4.JPG"
+  },
+  {
+    "revision": "5b5c89a3cee096bb34e38bb3205f421e",
+    "url": "/Bosancica/static/media/3.5b5c89a3.JPG"
+  },
+  {
+    "revision": "9617ca35b969c26220c9e0c9f6e7b8d0",
+    "url": "/Bosancica/static/media/3.9617ca35.JPG"
+  },
+  {
+    "revision": "d792e61a72bece7ef569a4814b43df70",
+    "url": "/Bosancica/static/media/30.d792e61a.JPG"
+  },
+  {
+    "revision": "ec9e6fff1ac3fa228d9a42b23e22e599",
+    "url": "/Bosancica/static/media/31.ec9e6fff.JPG"
+  },
+  {
+    "revision": "945c79084709fc5bb5660d26db7c7d1f",
+    "url": "/Bosancica/static/media/32.945c7908.JPG"
+  },
+  {
+    "revision": "50dad44d1dd788ef2c95f328fb67a403",
+    "url": "/Bosancica/static/media/33.50dad44d.JPG"
+  },
+  {
+    "revision": "1b3dc0126b30c44e60bf0da83f6122de",
+    "url": "/Bosancica/static/media/34.1b3dc012.JPG"
+  },
+  {
+    "revision": "38eabbe65c0ff0eb9744e63679e2b726",
+    "url": "/Bosancica/static/media/35.38eabbe6.JPG"
+  },
+  {
+    "revision": "819515a6f3bf8d2245fd26df277aa987",
+    "url": "/Bosancica/static/media/36.819515a6.JPG"
+  },
+  {
+    "revision": "ce2befb832ebef0687d0418bece7bd39",
+    "url": "/Bosancica/static/media/37.ce2befb8.JPG"
+  },
+  {
+    "revision": "1be108d7376ff0d8b2482f96cbcb871c",
+    "url": "/Bosancica/static/media/38.1be108d7.JPG"
+  },
+  {
+    "revision": "491997c66c554d50e7da1e93aa7432f0",
+    "url": "/Bosancica/static/media/39.491997c6.JPG"
+  },
+  {
+    "revision": "418bb1a5e01d0ac6025265e6021c9514",
+    "url": "/Bosancica/static/media/4.418bb1a5.JPG"
+  },
+  {
+    "revision": "441251de064e242ba2e85be1c6c2008a",
+    "url": "/Bosancica/static/media/4.441251de.jpg"
+  },
+  {
+    "revision": "8ff77aef2e781d9512d3c39b253be651",
+    "url": "/Bosancica/static/media/4.8ff77aef.jpg"
+  },
+  {
+    "revision": "25415b84fdfd7086091e6530edc353f7",
+    "url": "/Bosancica/static/media/40.25415b84.JPG"
+  },
+  {
+    "revision": "99b4f590754d718c7b8e291848a49f69",
+    "url": "/Bosancica/static/media/41.99b4f590.JPG"
+  },
+  {
+    "revision": "d5008874d83860599a2ea8b47ef4febf",
+    "url": "/Bosancica/static/media/42.d5008874.JPG"
+  },
+  {
+    "revision": "47a0b44b1cabace46e81d78378e76064",
+    "url": "/Bosancica/static/media/43.47a0b44b.JPG"
+  },
+  {
+    "revision": "98789567280e46862a41ca2adfb81e7c",
+    "url": "/Bosancica/static/media/44.98789567.JPG"
+  },
+  {
+    "revision": "cad24e27910832c5b7348ad3113cbb84",
+    "url": "/Bosancica/static/media/45.cad24e27.JPG"
+  },
+  {
+    "revision": "9aea9ccec9d73df48bf7b01ae11d5ddc",
+    "url": "/Bosancica/static/media/46.9aea9cce.JPG"
+  },
+  {
+    "revision": "7b754f993c912fd46fcd1260ebfc6210",
+    "url": "/Bosancica/static/media/47.7b754f99.JPG"
+  },
+  {
+    "revision": "d2d708d56019235b92af7baedc77fb4b",
+    "url": "/Bosancica/static/media/48.d2d708d5.JPG"
+  },
+  {
+    "revision": "8ff77aef2e781d9512d3c39b253be651",
+    "url": "/Bosancica/static/media/49.8ff77aef.jpg"
+  },
+  {
+    "revision": "5b5c89a3cee096bb34e38bb3205f421e",
+    "url": "/Bosancica/static/media/5.5b5c89a3.JPG"
+  },
+  {
+    "revision": "22a374d75e87712a1c7cb348bda18d42",
+    "url": "/Bosancica/static/media/6.22a374d7.JPG"
+  },
+  {
+    "revision": "06b4aafd128a7ba6f9d66aa565db0820",
+    "url": "/Bosancica/static/media/7.06b4aafd.JPG"
+  },
+  {
+    "revision": "cc3f8ca3cf1c77bc0a8616b88bde2987",
+    "url": "/Bosancica/static/media/8.cc3f8ca3.JPG"
+  },
+  {
+    "revision": "e283fe38f33d1233d731ccbe38e8eebe",
+    "url": "/Bosancica/static/media/9.e283fe38.JPG"
+  },
+  {
+    "revision": "dc4d2a1eccaff153a4a24415453e39fc",
+    "url": "/Bosancica/static/media/home_1.dc4d2a1e.jpg"
+  },
+  {
+    "revision": "a06c53280646c21d68fb36739ead7980",
+    "url": "/Bosancica/static/media/home_3.a06c5328.JPG"
+  },
+  {
+    "revision": "b14b0417b4c8e5833e7d336ab44198bf",
+    "url": "/Bosancica/static/media/home_4.b14b0417.JPG"
+  },
+  {
+    "revision": "742a0556659405fa3acdc21e9c508476",
+    "url": "/Bosancica/static/media/mm1.742a0556.jpg"
+  },
+  {
+    "revision": "aa91b58c2e744946d7b204e051f71109",
+    "url": "/Bosancica/static/media/mm2.aa91b58c.jpg"
+  },
+  {
+    "revision": "69e4a96284d5a769b3b71409671a6837",
+    "url": "/Bosancica/static/media/mm3.69e4a962.jpg"
+  },
+  {
+    "revision": "c3fee170768c2d48cf7642d4d2274492",
+    "url": "/Bosancica/static/media/mm4.c3fee170.jpg"
+  },
+  {
+    "revision": "760fbb1d624dbfebf910b2194f3437aa",
+    "url": "/Bosancica/static/media/mm5.760fbb1d.jpg"
+  },
+  {
+    "revision": "8dfff584548d31ff9e6ffd0b113a7aa2",
+    "url": "/Bosancica/static/media/mm6.8dfff584.jpg"
+  },
+  {
+    "revision": "f598df33c999444dd196afe01b64caaa",
+    "url": "/Bosancica/static/media/mm7.f598df33.jpg"
+  },
+  {
+    "revision": "bf656c5388f8babfbfd731ee4ee806bd",
+    "url": "/Bosancica/static/media/poster_placeholder.bf656c53.jpg"
+  }
+]);
